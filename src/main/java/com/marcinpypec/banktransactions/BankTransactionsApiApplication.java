754bed53c0025e62d422ea19ae4c19614bc5b05b
Marcin Pypeć <marcin.pypec@mytimeplan.com>
2026-02-04 10:34:46 +0100

    Initial project setup
    yaml

---
+++ src/main/java/com/marcinpypec/banktransactions/BankTransactionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcinpypec.banktransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankTransactionsApiApplication.class, args);
+    }
+
+}
